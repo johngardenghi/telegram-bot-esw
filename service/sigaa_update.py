861daa835f2feea4179993fe5031fd42e4331955
John L. Gardenghi <john.gardenghi@unb.br>
2026-05-12 14:57:51 -0300

    Adjustando indicação de orientação de estágio e atualização SIGAA

--- service/sigaa_update.py
+++ service/sigaa_update.py
@@ -68,9 +68,9 @@ class SIGAAUpdate:
             driver.find_element (By.XPATH, xpath).click()
 
             # Clica no primeiro botão "Continuar"
-            # xpath = '//*[@id="j_id_jsp_933481798_1"]/div/input'
-            # wait.until(EC.presence_of_element_located((By.XPATH, xpath)))
-            # driver.find_element (By.XPATH, xpath).click()
+            xpath = '//*[@id="j_id_jsp_933481798_1"]/div/input'
+            wait.until(EC.presence_of_element_located((By.XPATH, xpath)))
+            driver.find_element (By.XPATH, xpath).click()
 
             # Clica no Portal Coord. Graduação
             print("Acessando Portal Coord. Graduacao")
